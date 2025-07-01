020ceb57078ed04d2b096c56866b6e8beec270d5
alisayed2008 <50786141+alisayed2008@users.noreply.github.com>
2025-07-02 01:03:58 +0300

    layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="">
       <body className={cairo.className}>
+    <body import Script from "next/script";>
         {children}
         <Footer />
       </body>
